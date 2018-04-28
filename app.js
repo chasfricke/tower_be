1f52a720e6a857bdf6f3555dd8998cf086e8095e
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-28 11:09:26 -0600

    get works in postman

--- app.js
+++ app.js
@@ -1,8 +1,85 @@
-const express = require('express');
-const app = express();
-// const cors = require('cors');
-// // const bodyParser = require(“body-parser”);
-// app.use(cors());
-// // app.use(bodyParser.json());
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 
-module.exports = app;
+// app.use(cors({origin: true}))
+// app.use(bodyParser.json());
+
+// app.use("/locations", require('./routes/locations'))
+
+// app.use((request, response) => {
+//     response.status(404).send({message: 'url not found'});
+// });
+
+// app.use((err, req, res, next) => {
+//     console.error('ERROR:', err);
+//     res.status(500).send({message: err.message, stack: err.stack})
+// });
+
+// module.exports = app;
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const queries = require('./db/queries_locations')
+const bodyParser = require('body-parser')
+const database = require('./db/database-connection')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+  queries.list('locations').catch(console.error)
+})
+
+app.get('/locations', (request, response) => {
+  queries
+    .list('locations')
+    .then(locations => {
+      response.json({ locations })
+    })
+    .catch(console.error)
+})
+
+app.get('/locations/:id', (request, response) => {
+  queries
+    .read('locations', request.params.id)
+    .then(locations => {
+      locations ? response.json({ locations }) : response.sendStatus(404)
+    })
+    .catch(console.error)
+})
+
+app.post('/locations', (request, response) => {
+  queries
+    .create('locations', request.body)
+    .then(locations => {
+      response.status(201).json({ locations: locations })
+    })
+    .catch(console.error)
+})
+
+app.delete('/locations/:id', (request, response) => {
+  queries
+    .delete('locations', request.params.id)
+    .then(() => {
+      response.sendStatus(204)
+    })
+    .catch(console.error)
+})
+
+app.put('/locations/:id', (request, response) => {
+  queries
+    .update('locations', request.params.id, request.body)
+    .then(locations => {
+      response.json({ locations: locations[0] })
+    })
+    .catch(console.error)
+})
+
+app.use((request, response) => {
+  response.send(404)
+})
+
+module.exports = app
---
+++ routes/locations.js
@@ -0,0 +1,38 @@
+const app = require("express").Router();
+const queries = require("../db/queries_locations");
+
+
+app.get("/", (request, response, next) => {
+    queries.list().then(data => {
+      console.log('data', data);
+        response.json(data);
+    }).catch(next);
+});
+
+app.get("/:id", (request, response, next) => {
+    queries.read(request.params.id).then(locations => {
+        locations
+            ? response.json({locations})
+            : response.sendStatus(404)
+    }).catch(next);
+});
+
+app.post("/", (request, response, next) => {
+    queries.create(request.body).then(locations => {
+        response.status(201).json({locations});
+    }).catch(next);
+});
+
+app.delete("/:id", (request, response, next) => {
+    queries.delete(request.params.id).then(() => {
+        response.sendStatus(204);
+    }).catch(next);
+});
+
+app.put("/:id", (request, response, next) => {
+    queries.update(request.params.id, request.body).then(locations => {
+        response.json({locations});
+    }).catch(next);
+});
+
+module.exports = app
